efb7262a559c87f716474e9d939cb43d2d9611fd
John Dzak <j-dzak@northwestern.edu>
2013-11-22 13:38:57 -0600

    Change script to find Mechanism of Action.

--- run.rb
+++ run.rb
@@ -76,7 +76,8 @@ class DrugDefinitionResource
     concepts = []
     if full_concept = json['fullConcept']
       if group_roles = full_concept['groupRoles']
-        if roles = group_roles && group_roles.map.map{ |gr| gr['role'] }
+        group_roles.compact!
+        if roles = group_roles && group_roles.map{ |gr| gr['role'] }
           if roles
             roles.flatten!
             refined_roles = roles.select{ |r| r['roleName'] =~ /has_MoA/i }
@@ -97,44 +98,29 @@ class DrugDefinitionResource
 end
 
 resource = DrugDefinitionResource.new
-cui = 'C0004482'
-child = resource.find_first_concept_by_cui(cui)
-nui = child[:nui]
 
-role_concepts = resource.find_role_concepts_by_nui_and_role(nui, 'MECHANISM_OF_ACTION_KIND')
+dmard_cuis = %w(C0004482 C0010592 C0020336 C0063041 C0025677 C0036078 C1609165)
+gluc_cuis = %w(C0025815 C0032950 C0032952)
+nsaid_cuis = %w(C0083381 C0538927 C0762662 C0913246 C0972314 C0012091 C0022635 C0027396 C0972314)
+tnf_cuis = %w(C1619966 C1122087 C0245109 C0717758 C0666743 C0393022 C1609165)
 
-moa_tree = role_concepts.inject([]) do |memo, c|
-  memo << c
-  memo += resource.find_parent_concepts_by_nui(c[:nui])
-  memo
+cuis = dmard_cuis + gluc_cuis + nsaid_cuis + tnf_cuis
+
+cuis.each do |cui|
+  child = resource.find_first_concept_by_cui(cui)
+  if child
+    nui = child[:nui]
+
+    role_concepts = resource.find_role_concepts_by_nui_and_role(nui, 'MECHANISM_OF_ACTION_KIND')
+
+    moa_tree = role_concepts.inject([]) do |memo, c|
+      memo << c
+      memo += resource.find_parent_concepts_by_nui(c[:nui])
+      memo
+    end
+
+    puts "#{child[:name]} (#{cui}) has MoA ancestors: #{moa_tree.map{ |c| c[:name] }}"
+  else
+    puts "CUI not found: #{cui}"
+  end
 end
-
-puts "moa_tree: #{moa_tree.map{ |c| c[:name] }}"
-# puts "role_concepts: #{role_concepts}"
-# moa_tree << role_concepts.map do |rc|
-#   resource.find_parent_concepts_by_nui(rc[:nui])
-# end
-
-
-# dmard_cuis = %w(C0004482 C0010592 C0020336 C0063041 C0025677 C0036078 C1609165)
-# gluc_cuis = %w(C0025815 C0032950 C0032952)
-# nsaid_cuis = %w(C0083381 C0538927 C0762662 C0913246 C0972314 C0012091 C0022635 C0027396 C0972314)
-# tnf_cuis = %w(C1619966 C1122087 C0245109 C0717758 C0666743 C0393022 C1609165)
-
-# cuis = dmard_cuis + gluc_cuis + nsaid_cuis + tnf_cuis
-
-# cuis.each do |cui|
-#   child = resource.find_first_concept_by_cui(cui)
-
-#   if child
-#     nui = child[:nui]
-
-#     if nui
-#       concepts = resource.find_parent_concepts_by_nui(nui)
-
-#       puts "#{child[:name]} (#{cui}) has ancesstors: #{concepts.map{ |c| c[:name] }}"
-#     end
-#   else
-#     puts "CUI #{cui} not found"
-#   end
-# end
